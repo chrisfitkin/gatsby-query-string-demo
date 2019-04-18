0af294e57c295b960ce9565d370b9637d31d52f6
Chris Fitkin <cfitkin@gmail.com>
2019-04-18 10:53:31 -0700

    Add superhero images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Query String Demo`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Demonstrates dynamic content based on query string values in Gatsby.`,
+    author: `@chrisfitkin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,70 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Superman from "../components/superman"
+import Batman from "../components/batman"
+import WonderWoman from "../components/wonderWoman"
 import SEO from "../components/seo"
+import withLocation from "../components/withLocation"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Dynamic Content Based on Query String Values</h1>
-    <p>
-      <strong>GitHub:</strong>{" "}
-      <a
-        href="https://github.com/chrisfitkin/gatsby-query-string-demo/"
-        target="_blank"
-      >
-        https://github.com/chrisfitkin/gatsby-query-string-demo/
-      </a>
-    </p>
-    <p>
-      <strong>Instructions:</strong>{" "}
-      <a
-        href="https://medium.com/@chrisfitkin/how-to-get-query-string-parameter-values-in-gatsby-f714161104f"
-        target="_blank"
-      >
-        https://medium.com/@chrisfitkin/how-to-get-query-string-parameter-values-in-gatsby-f714161104f
-      </a>
-    </p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <ul>
-      <li>
-        <Link to="/?custom=A">Show A</Link>
-      </li>
-      <li>
-        <Link to="/?custom=B">Show B</Link>
-      </li>
-      <li>
-        <Link to="/?custom=C">Show C</Link>
-      </li>
-    </ul>
-    <hr />
-    <p>
-      <Link to="/simple?custom=hello">A Simple Example Page</Link>
-    </p>
-    <hr />
-  </Layout>
-)
+const IndexPage = ({ search }) => {
+  const { hero } = search
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <h1>Dynamic Content Based on Query String Values</h1>
+      <p>
+        <strong>GitHub:</strong>{" "}
+        <a
+          href="https://github.com/chrisfitkin/gatsby-query-string-demo/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://github.com/chrisfitkin/gatsby-query-string-demo/
+        </a>
+      </p>
+      <p>
+        <strong>Instructions:</strong>{" "}
+        <a
+          href="https://medium.com/@chrisfitkin/how-to-get-query-string-parameter-values-in-gatsby-f714161104f"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://medium.com/@chrisfitkin/how-to-get-query-string-parameter-values-in-gatsby-f714161104f
+        </a>
+      </p>
+      <h3>Who is your favorite hero?</h3>
+      <ul>
+        <li>
+          <Link to="/?hero=superman">Superman</Link>
+        </li>
+        <li>
+          <Link to="/?hero=batman">Batman</Link>
+        </li>
+        <li>
+          <Link to="/?hero=wonder_woman">Wonder Woman</Link>
+        </li>
+      </ul>
+      {hero && <h3>{hero}</h3>}
+      <div style={{ maxHeight: `400px`, marginBottom: `1.45rem` }}>
+        {(() => {
+          switch (hero) {
+            case "superman":
+              return <Superman />
+            case "batman":
+              return <Batman />
+            case "wonder_woman":
+              return <WonderWoman />
+          }
+        })()}
+      </div>
+      <hr />
+      <p>
+        <Link to="/simple?custom=hello">A Simple Example Page</Link>
+      </p>
+      <hr />
+    </Layout>
+  )
+}
 
-export default IndexPage
+export default withLocation(IndexPage)
